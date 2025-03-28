fe2b396ffbd4f6937b64ea529462b54430c28ca7
Jordan Ehara <jordan.ehara@gmail.com>
2025-03-27 20:49:17 -1000

    feat: stop bird from moving when colliding with the ground

--- bird.gd
+++ bird.gd
@@ -3,16 +3,19 @@ extends CharacterBody2D
 const START_POS = Vector2(100, 400);
 const GRAVITY: int = 10;
 const FLAP_SPEED: int = -5;
-const falling: bool = false; # collided with something and is falling
+var active: bool = true;
 
 func _ready() -> void:
 	position = START_POS;
 	$flying_animation.play();
 
 func _physics_process(delta: float) -> void:
-	velocity.y += GRAVITY * delta;
-	set_rotation(deg_to_rad(velocity.y * 5));
-	move_and_collide(velocity);
+	if active:
+		velocity.y += GRAVITY * delta;
+		set_rotation(deg_to_rad(velocity.y * 5));
+		move_and_collide(velocity);
+	else:
+		$flying_animation.stop();
 
 func flap() -> void:
 	velocity.y = FLAP_SPEED;
--- main.gd
+++ main.gd
@@ -1,10 +1,11 @@
 extends Node
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_SPACE:
-			$bird.flap();
 
+func _input(event: InputEvent) -> void:
+	if $bird.active:
+		if event is InputEventKey and event.pressed:
+			if event.keycode == KEY_SPACE:
+				$bird.flap();
 
 func _on_ground_hit() -> void:
-	pass
+	$bird.active = false;
